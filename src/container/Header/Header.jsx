898ea584c31cc0eef67a3b385e0d0f291e6eb04d
Nathan Lou <natloudev@gmail.com>
2022-06-12 12:22:32 -0700

    files and folders

---
+++ src/container/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react' 
+import { motion } from 'framer-motion'
+
+import './Header.scss';
+
+const Header = () => {
+    return (
+        <div className="app__header app__flex" style={{ color: 'red' }}>
+            Header
+        </div>
+    )
+}
+
+export default Header
