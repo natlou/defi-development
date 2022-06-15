3ba7e26338f81f3a1954ea028be99be030bf04ca
Nathan Lou <natloudev@gmail.com>
2022-06-15 10:00:28 -0700

    finished links

--- src/container/Contact/Contact.jsx
+++ src/container/Contact/Contact.jsx
@@ -5,11 +5,14 @@ const Contact = () => {
 
   return (
     <div className="app__contact">
+        <head>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        </head>
         <ul>
-            <li><a href="#"><span><i className="fab fa-linkedin-f"></i></span>LinkedIn</a></li>
-            <li><a href="#"><span><i className="fab fa-youtube-f"></i></span>Youtube</a></li>
-            <li><a href="#"><span><i className="fab fa-github-f"></i></span>Github</a></li>
-            <li><a href="#"><span><i className="fab fa-email-f"></i></span>Email</a></li>
+            <li><a href="mailto: natlou@g.ucla.edu"><span><i class="fa-solid fa-envelope"></i></span>Email</a></li>
+            <li><a href="https://www.linkedin.com/in/nathanlou/" target="_blank" rel="noopener"><span><i className="fa-brands fa-linkedin"></i></span>LinkedIn</a></li>    
+            <li><a href="https://github.com/natlou" target="_blank" rel="noopener"><span><i class="fa-brands fa-github"></i></span>Github</a></li>
+            <li><a href="https://www.youtube.com/channel/UC3GulQFCOr-4Fllh_d6LylQ" target="_blank" rel="noopener"><span><i className="fa-brands fa-youtube"></i></span>Youtube</a></li>
         </ul>
     </div>
   );
