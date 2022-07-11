9000761a8619b2214785b8c24affeceab291ff9f
natlou <natloudev@gmail.com>
2022-07-10 17:29:22 -0700

    removed useless images and constants

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,6 +1,3 @@
-import email from '../assets/email.png';
-import mobile from '../assets/mobile.png';
-import api from '../assets/api.png';
 import cpp from '../assets/cpp.png';
 import git from '../assets/git.png';
 import javascript from '../assets/javascript.png';
@@ -18,20 +15,12 @@ import sql from '../assets/sql.png';
 import tailwind from '../assets/tailwind.jpg'
 import solidity from '../assets/solidity.png'
 
-import about01 from '../assets/about01.png';
-import about02 from '../assets/about02.png';
-import about03 from '../assets/about03.png';
-import about04 from '../assets/about04.png';
-
 import circle from '../assets/circle.svg';
 import spinner from '../assets/bg_spinner.svg';
 import divisor from '../assets/divisor.svg';
 
 
 export default {
-  email,
-  mobile,
-  api,
   cpp,
   git,
   javascript,
@@ -48,10 +37,6 @@ export default {
   sql,
   tailwind,
   solidity,
-  about01,
-  about02,
-  about03,
-  about04,
   circle,
   spinner,
   divisor,
